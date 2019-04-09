117bedc38740850ed93bb986afdf6e7a0d7844bb
Benjamin Humpherys <mooglinux@gmail.com>
2019-04-09 10:46:18 -0700

    Add support for reading structs of length greater than 1

--- DbgPack/struct_reader.py
+++ DbgPack/struct_reader.py
@@ -18,7 +18,11 @@ class BinaryStructReader(BufferedReader):
         return struct.unpack_from(fmt, self.read(size))
     
     def _read_Struct(self, s: struct.Struct):
-        return s.unpack_from(self.read(s.size))[0]
+        unpacked = s.unpack_from(self.read(s.size))
+        if len(unpacked) == 1:
+            return unpacked[0]
+        else:
+            return unpacked
     
     def uintLE(self):
         return self._read_Struct(self._uintLE)
