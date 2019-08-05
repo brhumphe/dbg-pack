65db1b5161e1a4bb28a162acab10d21c8fb3e6e1
Rhett <reticentcool@protonmail.com>
2019-08-05 16:12:16 -0400

    Started refactoring. Still very WIP

--- DbgPack/abc.py
+++ DbgPack/abc.py
@@ -1,13 +1,38 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
 from typing import Dict
 
-from DbgPack import Asset
+
+class AbstractAsset(ABC):
+    name: str
+    path: Path
+
+    size: int
+    crc32: int
+
+    @property
+    @abstractmethod
+    def data(self) -> bytes:
+        pass
+
+    def __len__(self):
+        return self.size
 
 
 class AbstractPack(ABC):
-    path: str
-    assets: Dict[str, Asset]
-    
+    name: str
+    path: Path
+
+    asset_count: int
+    assets: Dict[str, AbstractAsset]  # Not sure if this will work correctly
+
     @abstractmethod
-    def __init__(self, path: str):
-        pass
+    def __init__(self, path: Path):
+        self.path = path
+        self.name = self.path.stem
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}("{self.path}")'
+
+    def __len__(self):
+        return self.asset_count
