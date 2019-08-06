73e735cb363b896ed01fee850b4b04ffd6aee041
Rhett <reticentcool@protonmail.com>
2019-08-05 22:38:06 -0400

    Refactored AssetManager

--- test/test_pack1.py
+++ test/test_pack1.py
@@ -1,4 +1,4 @@
-from DbgPack import Pack1
+from DbgPack import Pack1, AssetManager
 from pathlib import Path
 
 
@@ -21,14 +21,14 @@ def test_load_asset1():
     assert asset2.crc32 == 4092021062
 
 
-# def test_asset1_manager():
-#     manager = AssetManager(["sample.pack"])
-#     asset1 = manager['AbilityClasses.txt']
-#     assert asset1.offset == 8192
-#     assert len(asset1) == 149
-#     assert asset1.crc32 == 1748740018
-#
-#     asset2 = manager['Contrails_Vortex_Purple.xml']
-#     assert asset2.offset == 770682
-#     assert len(asset2) == 2563
-#     assert asset2.crc32 == 4092021062
+def test_asset1_manager():
+    manager = AssetManager([Path('sample.pack')])
+    asset1 = manager['AbilityClasses.txt']
+    assert asset1.offset == 8192
+    assert len(asset1) == 149
+    assert asset1.crc32 == 1748740018
+
+    asset2 = manager['Contrails_Vortex_Purple.xml']
+    assert asset2.offset == 770682
+    assert len(asset2) == 2563
+    assert asset2.crc32 == 4092021062
