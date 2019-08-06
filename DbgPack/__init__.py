1344cc8cd976256941af02d7ed9256527c1c3fb5
Rhett <reticentcool@protonmail.com>
2019-08-05 22:13:20 -0400

    Refactored Asset1 and Pack1

--- DbgPack/__init__.py
+++ DbgPack/__init__.py
@@ -1,7 +1,8 @@
+from .asset1 import Asset1
 from .asset2 import Asset2
 # from .asset_manager import AssetManager
-# from .pack1 import Pack1
+from .pack1 import Pack1
 from .pack2 import Pack2
 from .struct_reader import BinaryStructReader
 
-__all__ = ['Asset2', 'Pack2', 'BinaryStructReader']
+__all__ = ['Asset1', 'Asset2', 'Pack1', 'Pack2', 'BinaryStructReader']
--- test/test_pack1.py
+++ test/test_pack1.py
@@ -1,14 +1,15 @@
-from DbgPack import AssetManager, Pack1
+from DbgPack import Pack1
+from pathlib import Path
 
 
 def test_load_pack1():
-    pack = Pack1("sample.pack")
+    pack = Pack1(Path('sample.pack'))
     assert len(pack) == 235
-    assert pack.path == "sample.pack"
+    assert pack.path == Path('sample.pack')
 
 
 def test_load_asset1():
-    pack = Pack1("sample.pack")
+    pack = Pack1(Path('sample.pack'))
     asset1 = pack['AbilityClasses.txt']
     assert asset1.offset == 8192
     assert len(asset1) == 149
@@ -20,14 +21,14 @@ def test_load_asset1():
     assert asset2.crc32 == 4092021062
 
 
-def test_asset1_manager():
-    manager = AssetManager(["sample.pack"])
-    asset1 = manager['AbilityClasses.txt']
-    assert asset1.offset == 8192
-    assert len(asset1) == 149
-    assert asset1.crc32 == 1748740018
-    
-    asset2 = manager['Contrails_Vortex_Purple.xml']
-    assert asset2.offset == 770682
-    assert len(asset2) == 2563
-    assert asset2.crc32 == 4092021062
+# def test_asset1_manager():
+#     manager = AssetManager(["sample.pack"])
+#     asset1 = manager['AbilityClasses.txt']
+#     assert asset1.offset == 8192
+#     assert len(asset1) == 149
+#     assert asset1.crc32 == 1748740018
+#
+#     asset2 = manager['Contrails_Vortex_Purple.xml']
+#     assert asset2.offset == 770682
+#     assert len(asset2) == 2563
+#     assert asset2.crc32 == 4092021062
