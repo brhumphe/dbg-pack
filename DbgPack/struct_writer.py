b946fee4c9596b50a3183270aa6fa4f0af90b442
Rhett <reticentcool@protonmail.com>
2019-08-09 00:31:07 -0400

    Added struct_writer

---
+++ DbgPack/struct_writer.py
@@ -0,0 +1,43 @@
+from io import BufferedWriter
+from pathlib import Path
+import struct
+
+
+class BinaryStructWriter(BufferedWriter):
+    path: Path
+
+    # Big-endian structs:
+    _uint32BE = struct.Struct('>I')
+    _uint64BE = struct.Struct('>Q')
+
+    # Little-endian structs
+    _uint32LE = struct.Struct('<I')
+    _uint64LE = struct.Struct('<Q')
+
+    def _write_struct(self, s: struct.Struct, value):
+        self.write(s.pack(value))
+
+    def uint32LE(self, value):
+        return self._write_struct(self._uint32LE, value)
+
+    def uint32BE(self, value):
+        return self._write_struct(self._uint32BE, value)
+
+    def uint64LE(self, value):
+        return self._write_struct(self._uint64LE, value)
+
+    def uint64BE(self, value):
+        return self._write_struct(self._uint64BE, value)
+
+    def string(self, value: str, encoding='utf-8'):
+        self.write(value.encode(encoding))
+
+    def __init__(self, path: Path):
+        file_io = path.open('wb')
+        super().__init__(file_io)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
