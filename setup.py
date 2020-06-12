69fc6cca202d7ddb10b4e9b6c391a6a92827243b
Benjamin Humpherys <brhumphe@users.noreply.github.com>
2020-06-12 08:12:52 -0700

    Update setup.py with correct license classifier

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
         packages=setuptools.find_packages(),
         classifiers=[
                 "Programming Language :: Python :: 3",
-                "License :: OSI Approved :: MIT License",
+                "License :: OSI Approved :: GNU General Public License (GPL)",
                 "Operating System :: OS Independent",
         ],
 )
